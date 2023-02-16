e901dcab00bad0a5e5d2aa947bef52f39c3d62bb
ppost0 <p.post2321@yahoo.com>
2023-02-16 18:05:17 -0500

    Add icon through js

---
+++ src/index.js
@@ -0,0 +1,14 @@
+import './assets/styles/style.css';
+import './assets/styles/normalize.css';
+import Icon from './assets/images/todolist.svg';
+import { init } from './modules/taskFactory.js';
+
+function addIcon() {
+  const myIcon = new Image();
+  myIcon.src = Icon;
+  myIcon.classList.add('icon');
+
+  const title = document.getElementById('title');
+  header.insertBefore(myIcon, title);
+} ()
+
--- src/modules/index.js
+++
@@ -1,8 +0,0 @@
-import '../assets/styles/style.css';
-import '../assets/styles/normalize.css';
-import icon from '../assets/images/todolist.svg';
-import { init } from './taskFactory.js';
-
-function addIcon() {
-  const title = document.getElementById('title')
-}
