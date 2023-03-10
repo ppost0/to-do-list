54a56d2b03615e9288d5f5db1418003e219791f5
ppost0 <p.post2321@yahoo.com>
2023-03-10 13:20:19 -0500

    Get new project button working

--- src/index.js
+++ src/index.js
@@ -1,7 +1,6 @@
 import './assets/styles/style.css';
 import './assets/styles/normalize.css';
-// import {buildDefaultProject} from './modules/build-default-project.js';
-// import {renderDefaultProject} from './modules/dom-manip.js';
+
 
 
 const taskFactory = (title, description, dueDate, priority, checked) => {
@@ -15,9 +14,6 @@ function addTaskToProject(task, project) {
 }
 
 
-
-
-
 //create project list and default project: Personal
 let projectsList = {};
 projectsList['Personal'] = [];
@@ -27,17 +23,18 @@ function newProject(title) {
   projectsList[title] = [];
 }
 
-//render projects to list display
+//render projects to projects navlist display
 function renderProjects() {
+  let oldNodes = document.querySelectorAll('.project-list-project');
+  oldNodes.forEach((e) => {e.remove()});
   let projects = Object.keys(projectsList);
   for (let i = 0; i < projects.length; i++) {
     const navProjects = document.getElementById('navProjects');
     let current = document.createElement('div');
     current.textContent = projects[i];
+    current.classList.add('project-list-project');
     navProjects.appendChild(current);
-
   }
-
 }
 
 
@@ -51,30 +48,71 @@ function newTask() {
 }
 
 
-//attach event to open new item form
+//open new item form
 const navProjectsButton = document.getElementById('navProjectsButton');
 navProjectsButton.addEventListener('click', function() {
   const newItemForm = document.getElementById('newItemForm');
   newItemForm.style.display = 'grid';
+  const newProjectBtn = document.getElementById('newProjectBtn');
+  newProjectBtn.style.display = 'block';
+  const newTaskBtn = document.getElementById('newTaskBtn');
+  newTaskBtn.style.display = 'block';
+
+
 })
 
-//attach event to close new item form
+//close new item form
 const formCloseBtn = document.getElementById('formCloseBtn');
 formCloseBtn.addEventListener('click', function(e) {
   e.target.parentElement.style.display = 'none';
 })
 
-//attach event for new project button, should clear form and add inputs for new project function
+
+
+//new project button, should clear form and add inputs for new project function
 const newProjectBtn = document.getElementById('newProjectBtn');
 newProjectBtn.addEventListener('click', function(e) {
   const newItemForm = document.getElementById('newItemForm');
-  document.getElementById('newProjectBtn').remove();
-  document.getElementById('newTaskBtn').remove();
+  document.getElementById('newProjectBtn').style.display = 'none';
+  document.getElementById('newTaskBtn').style.display = 'none';
+
+  //title for new project
+  let title = document.createElement('input');
+  title.setAttribute('type', 'text');
+  title.setAttribute('name', 'title');
+  title.setAttribute('placeholder', 'New Project Title');
+
+  //submit button
+  let button = document.createElement('button');
+  button.textContent = 'Submit';
+  button.style.height = '50px';
+  button.style.width = '100px';
+  button.type = 'button';
+
+  //click submit button
+  button.addEventListener('click', function(a) {
+    if (title.value.length > 1) {
+      newProject(title.value);
+      title.value = '';
+      renderProjects();
+    }
+  })
+
+
+
+  newItemForm.appendChild(title);
+  newItemForm.appendChild(button);
+})
+
+//attach event for new task button, should clear form and add inputs for new task function
+const newTaskBtn = document.getElementById('newTaskBtn');
+newTaskBtn.addEventListener('click', function(e) {
+  const newItemForm = document.getElementById('newItemForm');
+  document.getElementById('newProjectBtn').style.display = 'none';
+  document.getElementById('newTaskBtn').style.display = 'none';
 
 
 })
-
-
 
 
 
@@ -89,7 +127,7 @@ let myTask2 = taskFactory('Buy clothes', '', new Date(2023, 3, 22, 'LOW', false)
 
 projectsList['Personal'].push(myTask1);
 
-console.log(projectsList, 'Projects List');
+console.table('Projects List: ', projectsList);
 
 addTaskToProject(myTask2, 'Work');
 
